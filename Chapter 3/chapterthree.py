40d93ee3c0da5fda7af9ef732f17b5d51d32d6b9
Michael Brown <mikeabrownjr+github@gmail.com>
2021-11-15 16:04:10 -0500

    spacing

--- Chapter 3/chapterthree.py
+++ Chapter 3/chapterthree.py
@@ -1,6 +1,3 @@
-from typing import Any
-
-
 names = ['Michael', 'Fatima', 'Joy', 'Geraldine']
 
 print('Lets add some more names to the list. Add a name: ')
@@ -9,4 +6,3 @@ newName = input()
 names.append(newName)
 print('There are now ', len(names), ' names in the list.')
 print('The new name added was,', newName)
-
