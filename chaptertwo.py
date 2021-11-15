71f8ccedf5f2b9eeedb86aee997a8963b1b986c9
Michael Brown <mikeabrownjr+github@gmail.com>
2021-11-15 12:21:54 -0500

    Added a logic error module at 2.7.5

--- chaptertwo.py
+++ chaptertwo.py
@@ -3,3 +3,13 @@ weeks = 50
 hourly_wage = int(input('Enter hourly wage: '))
 
 print('\nSalary is', hourly_wage * hours * weeks)
+
+num_beans = 500
+num_jars = 3
+total_beans = 0
+
+print(num_beans, 'beans in', end=' ')
+print(num_jars, 'jars yields',end=' ')
+total_beans = num_beans * num_jars
+print(total_beans, 'total')
+
