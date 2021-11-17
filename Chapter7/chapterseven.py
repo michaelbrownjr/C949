22babbceb9b7f1dc950d9c984d5ae4abc643aaa7
Michael Brown <mikeabrownjr+github@gmail.com>
2021-11-17 16:36:44 -0500

    fixed minus methods and class customizations

--- Chapter7/chapterseven.py
+++ Chapter7/chapterseven.py
@@ -1,7 +1,7 @@
 class Time:
-    def __init__(self):
-        self.hours = 0
-        self.minutes = 0
+    def __init__(self, hours, minutes):
+        self.hours = hours
+        self.minutes = minutes
 
     def add_minutes(self, minutes_to_add):
         self.minutes += minutes_to_add
@@ -10,37 +10,37 @@ class Time:
         self.hours += hours_to_add
     
     def minus_minutes(self, minutes_to_minus):
-        self.minutes += minutes_to_minus
+        self.minutes -= minutes_to_minus
     
     def minus_hours(self, hours_to_minus):
-        self.hours += hours_to_minus
+        self.hours -= hours_to_minus
 
-    def print_time(self):
-        print('The time is %d:%d' % (self.hours,self.minutes))
+    def __str__(self):
+        return('The time is %d:%d' % (self.hours,self.minutes))
 
-my_time = Time()
-my_time.hours = 7
-my_time.minutes = 15
+my_time = Time(7, 15)
+print(my_time)
 
-my_time.print_time()
 print('What do you want to do?')
 user_input = int(input('1. Add minutes\n2. Add hours\n3. Remove minutes\n4. Remove hours\n'))
 
 if user_input == 1:
     minutes_to_add = int(input('How many minutes do you want to add?\n'))
     my_time.add_minutes(minutes_to_add)
-    my_time.print_time()
+    print(my_time)
 elif user_input == 2:
     hours_to_add = int(input('How many hours do you want to add?\n'))
     my_time.add_hours(hours_to_add)
-    my_time.print_time()
+    print(my_time)
 elif user_input == 3:
     minutes_to_minus = int(input('How many minutes do you want to minus?\n'))
     my_time.minus_minutes(minutes_to_minus)
-    my_time.print_time()
+    print(my_time)
 elif user_input == 4:
     hours_to_minus = int(input('How many hours do you want to minus?\n'))
     my_time.minus_hours(hours_to_minus)
-    my_time.print_time()
+    print(my_time)
 else:
     print('No such option.')
+
+    
