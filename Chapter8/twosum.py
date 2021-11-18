fe090ccdd39c1fc8ea9dd9d31d39808fc1c8d6eb
Michael Brown <mikeabrownjr+github@gmail.com>
2021-11-18 16:34:29 -0500

    Added intro to problem.

--- Chapter8/twosum.py
+++ Chapter8/twosum.py
@@ -1,3 +1,7 @@
+# Easy problem from leetcode
+
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
 class Solution:
     def __init__(self, nums, target) -> None:
         self.nums = nums
