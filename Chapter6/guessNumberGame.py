79a3d0ece4642dd243977207ab25d880c180653c
Michael Brown <mikeabrownjr+github@gmail.com>
2021-11-18 16:57:03 -0500

    Changed to have the computer guess between 1 & num

--- Chapter6/guessNumberGame.py
+++ Chapter6/guessNumberGame.py
@@ -4,7 +4,7 @@ import random
 numToGuess = int(input('Enter the number the computer has to guess:'))
 
 def guessingGame(num):
-    guessedNum = random.randint(1,10)
+    guessedNum = random.randint(1,numToGuess)
     computerGuess = False
 
     while computerGuess != True:
@@ -13,7 +13,7 @@ def guessingGame(num):
            computerGuess = True
        else: 
             print('%d is not correct! Guess again!' % guessedNum)
-            guessedNum = random.randint(1,10)
+            guessedNum = random.randint(1,numToGuess)
     
     # O(n)
     # for guessedNum in range(1,10):
@@ -22,5 +22,6 @@ def guessingGame(num):
     #         guessedNum += 1
     #     else:
     #         return('That is correct! You guessed %d and the number was %d!' % (guessedNum, num))
+    
 
 guessingGame(numToGuess)
