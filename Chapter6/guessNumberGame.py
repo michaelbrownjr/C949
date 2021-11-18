0d56874792d897dfd5690d71356c47fdf5d51341
Michael Brown <mikeabrownjr+github@gmail.com>
2021-11-18 12:04:54 -0500

    Added while loop and random module

--- Chapter6/guessNumberGame.py
+++ Chapter6/guessNumberGame.py
@@ -4,18 +4,23 @@ import random
 numToGuess = int(input('Enter the number the computer has to guess:'))
 
 def guessingGame(num):
-    #guessedNum = random.randint(1,10)
+    guessedNum = random.randint(1,10)
+    computerGuess = False
 
-    #while guessedNum != num:
-     #   print('%d is not correct! Guess again!' % guessedNum)
-      #  guessedNum = random.randint(1,10)
+    while computerGuess != True:
+       if guessedNum == num:
+           print('%d is correct!' % guessedNum)
+           computerGuess = True
+       else: 
+            print('%d is not correct! Guess again!' % guessedNum)
+            guessedNum = random.randint(1,10)
     
     # O(n)
-    for guessedNum in range(1,10):
-        if guessedNum != num:
-            print('%d is not correct! Guess again!' % guessedNum)
-            guessedNum += 1
-        else:
-            return('That is correct! You guessed %d and the number was %d!' % (guessedNum, num))
+    # for guessedNum in range(1,10):
+    #     if guessedNum != num:
+    #         print('%d is not correct! Guess again!' % guessedNum)
+    #         guessedNum += 1
+    #     else:
+    #         return('That is correct! You guessed %d and the number was %d!' % (guessedNum, num))
 
-print(guessingGame(numToGuess))
+guessingGame(numToGuess)
